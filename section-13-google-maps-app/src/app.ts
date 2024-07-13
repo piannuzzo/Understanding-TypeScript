a60b82a46e887567a0da744c7500a6fdbe243348
Peter Iannuzzo <piannuzzo@users.noreply.github.com>
2024-07-13 18:43:24 -0400

    initial commit of section 13, google maps app

---
+++ section-13-google-maps-app/src/app.ts
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+// const cL = console.log
+
+const form = document.querySelector('form')! as HTMLFormElement
+const addressInput = document.getElementById('address')! as HTMLInputElement
+const GOOGLE_API_KEY = 'AIzaSyCsfoUapu6Ar18MlCRsa9N49cLauG9S1Sk'
+
+type GoogleGeocodingResponse = {
+	results: {
+		geometry: {
+			location: { lat: number, lng: number }
+		}
+	}[],
+	status: 'OK' | 'ZERO_RESULTS'
+}
+
+// not needed as @types/googlemaps package has been installed
+// declare var google: any
+
+function searchAddressHandler(event: Event) {
+	event.preventDefault()
+	const enteredAddress = addressInput.value
+
+	axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?
+		address=${encodeURI(enteredAddress)}
+		&key=${GOOGLE_API_KEY}`)
+		.then(response => {
+			if (response.data.status !== 'OK') {
+				throw new Error('Could not fetch location')
+			}
+			const coordinates = response.data.results[0].geometry.location
+			const map = new google.maps.Map(document.getElementById("map")!, {
+				center: coordinates,
+				zoom: 16
+			})
+			new google.maps.Marker({position: coordinates, map: map})
+		})
+		.catch(error => {
+			alert(error.message)
+		})
+}
+
+form.addEventListener('submit', searchAddressHandler)
